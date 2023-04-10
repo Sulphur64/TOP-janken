e3ab40e5d5ad00c9426e64c0e39ec09886272e23
Sulphur64 <128920405+Sulphur64@users.noreply.github.com>
2023-04-10 16:10:18 +0200

    V1 created a new (), removed non working stuff
    html and css corrected a bit
    js : remove playerselection and use foreach()
    wrote the process
    added a function : symbolPotency defines the strengh
    symbolforce apply it to players symbols
    
    corrected outcome to take = as a result

--- javascript/Janken.js
+++ javascript/Janken.js
@@ -1,51 +1,46 @@
-let playerInput = playerSelection(); //document.querySelector(".rock" || ".paper" || ".scissors").addEventListener("onclick", playerSelection);//queryblabla if button 1 or 2 or 3 is pressed = true
 let playerChoice; 
 let botChoice;
+let playerValue;
+let botValue;
 
 let paper;
 let scissors;
 let rock;
 
-// functions needed : computerChoice, playerChoice, symbolForce and outcome
-// process : when playerChoice is made, computer choice is generated and symbolForce defined then result happens
+let symbolValue;
 
 
+let playerInput = document.querySelectorAll("button"); // doit return true et lancer la sélection
+playerInput.forEach((i) => {
+    i.addEventListener("click", (e) => {
+        playerChoice = e.target.value;
+        document.querySelector(".playerText").textContent = "You chose ! get ready";
 
+        computerChoice();
 
+        symbolPotency(playerChoice); //crée la force des symboles
+        symbolForce (playerChoice); // corrèle le string aux valeurs num
+        playerValue = symbolValue; // donne la force du symbole au joueur
+        
 
+        symbolForce (botChoice);
+        botValue = symbolValue;
 
+        outcome(playerValue,botValue);
+    })
+})
 
+function computerChoice () { //botChoice generate a number between 1 and 99, each tier is a choice
+    let random = Math.floor(Math.random()*99);
 
-function playerSelection () { //can i use query directly here ? must initialise on press
-    if (document.querySelector(".rock")) {
-        playerInput.addEventListener("onclick",() => playerChoice = rock);
-        document.querySelector(".playerText").textContent = "You chose ! get ready"
+    if (random <= 33){
+        botChoice = "paper";
 
-    }else if (document.querySelector(".paper")){
-        playerInput.addEventListener("onclick",() => playerChoice = paper);
-
-    }else if (document.querySelector(".scissors")){ 
-        playerInput.addEventListener("onclick",() => playerChoice = scissors);
-    }
-    
-
-}
-
-console.log (`player pressed : ${playerChoice}`)
-
-
-
-function computerChoice () { //botChoice must be generated when player chose
-    let random = Math.floor(math.random()*3);
-
-    if (random < 2){
-        botChoice = paper;
-
-    } else if ((random > 2) && (random < 3)){
-        botChoice = scissors;
+    } else if ((random <= 66) &&(random >= 33)){
+        botChoice = "scissors";
 
     } else {
-        botChoice = rock;
+        botChoice = "rock";
 
     }
     //bot color turns bright red would be nice, check how to change color in the DOM
@@ -55,21 +50,24 @@ console.log(`botChoice : ${botChoice}`)
 
 
 
-function symbolForce (symbolName){ // i must now make bot and player use symbolforce to convert their choice value
+function symbolPotency (symbolName){ // define the value of each symbol when player enter his
 
     let paperWeight = 2;          //to be incremented by symbolForce
     let scissorsWeight = 2;
     let rockWeight = 2;
 
-    if  (symbolName === paper) {
-        scissors = scissorsWeight+1;
+    if  (symbolName === "paper") {
+        paper = paperWeight;
+        scissors = scissorsWeight + 1;
         rock = rockWeight - 1;
 
-    } else if (symbolName === scissors){
+    } else if (symbolName === "scissors"){
+        scissors = scissorsWeight;
         rock = rockWeight + 1;
         paper = paperWeight - 1;
 
-    } else {                //symbolName === rock
+    } else if (symbolName === "rock"){
+        rock = rockWeight;
         paper = paperWeight + 1;
         scissors = scissorsWeight - 1;
 
@@ -77,23 +75,38 @@ function symbolForce (symbolName){ // i must now make bot and player use symbolf
 
 }
 
-document.log(`rock: ${rock} paper: ${paper} scissors: ${scissors}`)
+console.log(`rock: ${rock} paper: ${paper} scissors: ${scissors}`)
 
+function symbolForce (choice) { //compare the string to the number
 
+    symbolValue=0;
 
-function outcome (botChoice,playerChoice){ // triggers the animation ROCK PAPER SCISSORS and launched when player press a button
-    
-    document.querySelector(".result").textContent ="ROCK";
-    setTimeout (1000);
-    document.querySelector(".result").textContent ="PAPER";
-    setTimeout (1000);
-    document.querySelector(".result").textContent ="SCISSORS";
-    setTimeout (1000);
-
-    if (playerChoice > botChoice){
-        document.querySelector(".result").textContent =`You win ! Your ${playerChoice} beat BOT's ${botChoice}, please refresh the page to play again`;// message WIN then refresh the page
+    if (choice === "rock"){
+        symbolValue = rock;
+    } else if (choice === "paper"){
+        symbolValue = paper;
+    } else if (choice === "scissors"){
+        symbolValue = scissors;
     }
-    else {
-        document.querySelector(".result").textContent =`BOT win ! Your ${playerChoice} is beaten by BOT's ${botChoice}, please refresh the page to play again`;
+}
+
+
+
+function outcome (num1, num2){ 
+
+    document.querySelector(".result").textContent ="ROCK";
+    setTimeout (3000);
+    document.querySelector(".result").textContent ="PAPER";
+    setTimeout (3000);
+    document.querySelector(".result").textContent ="SCISSORS";
+    setTimeout (3000);
+
+    if (num1 > num2){
+        document.querySelector(".result").textContent =`You win ! Your ${playerChoice} beat BOT's ${botChoice}!`;
+    }
+    else if (num1 = num2) {
+        document.querySelector(".result").textContent =`EX AEQUO ! Your ${playerChoice} equals BOT's ${botChoice}!`;
+    } else {
+        document.querySelector(".result").textContent =`BOT win ! Your ${playerChoice} is beaten by BOT's ${botChoice}! `;
     }
 }
