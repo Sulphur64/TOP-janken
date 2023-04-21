2c961c8fc6be8f39605f406e3864333ed970338d
Sulphur64 <128920405+Sulphur64@users.noreply.github.com>
2023-04-21 14:29:25 +0200

    day 1.5 finished the main room UI
    to do : do the challenge button

--- javascript/DOM.js
+++ javascript/DOM.js
@@ -6,6 +6,10 @@ const botSpeak = document.createElement("div")
 
 const lightPath = document.createElement("div");
 
+const trialRoomFloor= document.createElement("div");
+
+
+
 body.appendChild(gameWindow);
 
 gameFlow()
@@ -61,15 +65,66 @@ function botTalk (dialog){ //BOT speaking, handle the scores and matches too
         botSpeak.textContent="ENTER, HUMAN"
     } else if (dialog==2){
         gameWindow.appendChild(botSpeak)
+        botSpeak.textContent="IT IS TIME."
     }
 };
 
+function botEyeState (state) {
+
+    const botEye = document.createElement("div");
+    const botEyePupil = document.createElement("div");
+    botEyePupil.textContent="\u{2022}";
+    botEye.classList.add("bot-eye");
+    gameWindow.insertBefore(botEye,trialRoomFloor)
+    
+
+    if (state==1){ //wakes up {eye from black to red, pupil appear}
+        botEye.classList.remove("bot-eye");
+        botEye.classList.add("bot-eye-active");
+        botEyePupil.classList.add("bot-eye-pupil");
+        botEye.appendChild(botEyePupil);
+
+    } else if (state==2){ //woke, pupil focusing on the player
+        botEye.classList.remove("bot-eye");
+        botEye.classList.add("bot-eye-active");
+        botEyePupil.classList.add("bot-eye-pupil");
+        botEye.appendChild(botEyePupil);
+        botEye.setAttribute('style','align-items:"flex-end"')
+    }
+};
+
+function getRandomText(length) {
+    let result = '';
+    const characters = '  ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
 function trialRoom () { // main janken event, this will loop every game of 5
     lightPath.remove();
+
     gameWindow.classList.add("gamewindow-while-game");
-    const trialRoomFloor= document.createElement("div");
-    botTalk(2)
 
-
+    botTalk(2);
     
+    trialRoomFloor.classList.add("trialroom-floor");
+    gameWindow.appendChild(trialRoomFloor);
+    trialRoomFloor.textContent="\u{A19C}";
+
+    const trialRoomExitDoor= document.createElement("div")
+    trialRoomExitDoor.classList.add("trialroom-exit-door")
+    trialRoomFloor.prepend(trialRoomExitDoor)
+
+    botEyeState();
+
+    const runeWall = document.createElement("div")
+    runeWall.classList.add('runewall')
+    gameWindow.insertBefore(runeWall,trialRoomFloor)
+    runeWall.textContent=getRandomText(25000)
+
 }
